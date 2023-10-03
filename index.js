4a71d0e5f1dd5ab6902041df56df2508c225b09e
mkstamin <mkstamincse@gmail.com>
2023-10-03 12:30:04 +0600

    first commit

---
+++ index.js
@@ -0,0 +1,133 @@
+const express = require("express");
+const bodyParser = require("body-parser");
+const { createProductsTable, getConnection } = require("./schema");
+
+const app = express();
+const port = 3000;
+
+// Create products table if not exists
+createProductsTable();
+
+app.use(bodyParser.json());
+
+// Retrieve all products
+app.get("/products", async (req, res) => {
+  const connection = await getConnection();
+  try {
+    const [rows] = await connection.query("SELECT * FROM products");
+    res.json(rows);
+  } catch (error) {
+    console.error("Error retrieving products:", error);
+    res.status(500).json({ error: "Internal Server Error" });
+  } finally {
+    connection.release();
+  }
+});
+
+// Retrieve a product by ID
+app.get("/products/:id", async (req, res) => {
+  const productId = req.params.id;
+
+  const connection = await getConnection();
+  try {
+    const [rows] = await connection.query(
+      "SELECT * FROM products WHERE id = ?",
+      [productId]
+    );
+    if (rows.length === 0) {
+      res.status(404).json({ error: "Product not found" });
+    } else {
+      res.json(rows[0]);
+    }
+  } catch (error) {
+    console.error("Error retrieving product:", error);
+    res.status(500).json({ error: "Internal Server Error" });
+  } finally {
+    connection.release();
+  }
+});
+
+// Create a new product
+app.post("/products", async (req, res) => {
+  const {
+    name,
+    price,
+    image,
+    colors,
+    company,
+    description,
+    category,
+    shipping,
+  } = req.body;
+
+  const newColors = JSON.stringify(colors);
+
+  const connection = await getConnection();
+
+  try {
+    const [result] = await connection.query(
+      `INSERT INTO products (name, price, image, colors, company, description, category, shipping) VALUES ('${name}', '${price}', '${image}', '${newColors}', '${company}', '${description}', '${category}', ${shipping})`
+    );
+    res.json({
+      message: "Product added successfully",
+      productId: result.insertId,
+    });
+  } catch (error) {
+    console.error("Error adding product:", error);
+    res.status(500).json({ error: "Internal Server Error" });
+  } finally {
+    connection.release();
+  }
+});
+
+// Update a product by ID
+app.put("/products/:id", async (req, res) => {
+  const productId = req.params.id;
+  const { name, price } = req.body;
+  const connection = await getConnection();
+  try {
+    const [result] = await connection.query(
+      "UPDATE products SET name = ?, price = ? WHERE id = ?",
+      [name, price, productId]
+    );
+    if (result.affectedRows === 0) {
+      res.status(404).json({ error: "Product not found" });
+    } else {
+      res.json({ message: "Product updated successfully" });
+    }
+  } catch (error) {
+    console.error("Error updating product:", error);
+    res.status(500).json({ error: "Internal Server Error" });
+  } finally {
+    connection.release();
+  }
+});
+
+// Delete a product by ID
+app.delete("/products/:id", async (req, res) => {
+  const productId = req.params.id;
+
+  const connection = await getConnection();
+  try {
+    const [result] = await connection.query(
+      "DELETE FROM products WHERE id = ?",
+      [productId]
+    );
+    if (result.affectedRows === 0) {
+      res.status(404).json({ error: "Product not found" });
+    } else {
+      res.json({ message: "Product deleted successfully" });
+    }
+  } catch (error) {
+    console.error("Error deleting product:", error);
+    res.status(500).json({ error: "Internal Server Error" });
+  } finally {
+    connection.release();
+  }
+});
+
+// Error handling and other routes go here
+
+app.listen(port, () => {
+  console.log(`Server is running on port ${port}`);
+});
---
+++ schema.js
@@ -0,0 +1,47 @@
+const mysql = require("mysql2/promise");
+
+// Create a pool of connections to the database
+const pool = mysql.createPool({
+  host: "localhost",
+  user: "root",
+  password: "Mks@1234",
+  database: "grocery_db",
+  waitForConnections: true,
+  connectionLimit: 10,
+  queueLimit: 0,
+});
+
+// Create products table if not exists
+async function createProductsTable() {
+  const connection = await pool.getConnection();
+  try {
+    await connection.query(`
+      CREATE TABLE IF NOT EXISTS products (
+        id INT AUTO_INCREMENT PRIMARY KEY,
+        name VARCHAR(255) NOT NULL,
+        price DECIMAL(10, 2) NOT NULL,
+        colors VARCHAR(255) NOT NULL,
+        company VARCHAR(255) NOT NULL,
+        image VARCHAR(255) NOT NULL,
+        description TEXT NOT NULL,
+        category VARCHAR(255) NOT NULL,
+        shipping BOOLEAN NOT NULL
+      )
+    `);
+    console.log("Products table created successfully");
+  } catch (error) {
+    console.error("Error creating products table:", error);
+  } finally {
+    connection.release();
+  }
+}
+
+// Function to get a connection from the pool
+async function getConnection() {
+  return await pool.getConnection();
+}
+
+module.exports = {
+  createProductsTable,
+  getConnection,
+};
